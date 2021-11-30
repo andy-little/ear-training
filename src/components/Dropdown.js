b93328af6b93b778a1b81e1c402492bb934c419a
andy-little <69594249+andy-little@users.noreply.github.com>
2021-11-30 16:15:51 +0000

    tweaked piano and control appearance

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Dropdown = ({label, options, key_, setValue}) => {
     return (
-        <div className="select-container">
+        <div className="select-container pick-key">
             <label htmlFor={label}>{label}:</label>
             <select name={label} id={label} value={key_} onChange={(e)=>{setValue(e.target.value)}}>
                 {options.map((option)=>{
--- src/components/Piano.js
+++ src/components/Piano.js
@@ -5,16 +5,16 @@ const Piano = () => {
         <div className="piano-container">
             <ul>
                 <li className="white c"><span className="text">C</span></li>
-                <li className="black cs"><span className="text-black">C#</span></li>
+                <li className="black cs"><span className="text-black">Db<br/>C#</span></li>
                 <li className="white d"><span className="text">D</span></li>
-                <li className="black ds"><span className="text-black">D#</span></li>
+                <li className="black ds"><span className="text-black">Eb<br/>D#</span></li>
                 <li className="white e"><span className="text">E</span></li>
                 <li className="white f"><span className="text">F</span></li>
-                <li className="black fs"><span className="text-black">F#</span></li>
+                <li className="black fs"><span className="text-black">Gb<br/>F#</span></li>
                 <li className="white g"><span className="text">G</span></li>
-                <li className="black gs"><span className="text-black">G#</span></li>
+                <li className="black gs"><span className="text-black">Ab<br/>G#</span></li>
                 <li className="white a"><span className="text">A</span></li>
-                <li className="black as"><span className="text-black">A#</span></li>
+                <li className="black as"><span className="text-black">Bb<br/>A#</span></li>
                 <li className="white b"><span className="text">B</span></li>
             </ul>
         </div>
