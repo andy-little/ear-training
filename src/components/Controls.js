776ec9d5ece6a2355548451115aba1981e8cdbc2
andy-little <69594249+andy-little@users.noreply.github.com>
2021-12-02 17:57:51 +0000

    add notes button to interface and swapped help to header and replay to footer

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import {MdOutlineReplayCircleFilled} from 'react-icons/md'
+import {MdOutlineHelp} from 'react-icons/md'
 import { useEarTrainingContext } from '../EarTrainingContext'
 import Dropdown from './Dropdown'
 import TonalityToggle from './TonalityToggle'
-
+import HelpModal from './HelpModal';
 
 const Controls = () => {
-    const {keyOptions, key_, setKey_} = useEarTrainingContext();
+    const {keyOptions, key_, setKey_, isHelpOpen, setIsHelpOpen} = useEarTrainingContext();
    /*  console.log(key); */
     return (
         <div className="controls">
             <Dropdown label="Key" options={keyOptions} key_={key_} setValue={setKey_}/>
             <TonalityToggle/>
-            <MdOutlineReplayCircleFilled className="replay-btn"/>
+            
+            <MdOutlineHelp className="footer-help" onMouseEnter={()=>{setIsHelpOpen(true)}} onMouseLeave={()=>{setIsHelpOpen(false)}}/>
+            <HelpModal/>
         </div>
     )
 }
