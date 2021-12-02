2f38b93d640879cc4c08f56cacb7e34e18217e68
andy-little <69594249+andy-little@users.noreply.github.com>
2021-12-02 17:58:57 +0000

    exported keys object to external file

---
+++ src/data/keys.js
@@ -0,0 +1,7 @@
+export const keys = [
+    {"major": "C", "minor": 'Am'},
+    {"major": "G", "minor": 'Em'},
+    {"major": "D", "minor": 'Bm'},
+    {"major": "F", "minor": 'Dm'},
+    {"major": "Bb", "minor": 'Gm'}
+]
