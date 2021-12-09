1a807001b818ed399a4534340758a24854fa2ad3
andy-little <69594249+andy-little@users.noreply.github.com>
2021-12-09 17:15:49 +0000

    toggle between percent and ratio onclick score

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { useEarTrainingContext } from '../EarTrainingContext'
 
 
 const Score = () => {
     const {score, numQs} = useEarTrainingContext();
+    const [percent, setPercent] = useState(0);
+    const [isPercent, setIsPercent] = useState(false)
+    useEffect(() => {
+        setPercent(Math.floor((score/numQs) * 100));
+        
+    }, [numQs])
     return (
-        <div className="score-container">
+        <div className="score-container" onClick={()=>setIsPercent(!isPercent)}>
             <p className="score-text">
-                Score: {score}/{numQs}
+                Score: {isPercent ? `${percent}%`: `${score}/${numQs}`}
             </p>
         </div>
     )
