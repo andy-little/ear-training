21966b69f225987baabf4be248702ba44b6089ac
andy-little <69594249+andy-little@users.noreply.github.com>
2021-12-09 18:36:57 +0000

    fixed bug causing settings menu to not fit into viewport

--- src/components/EarTrainerFooter.js
+++ src/components/EarTrainerFooter.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import Score from "./Score";
 import {GiMusicalNotes} from 'react-icons/gi'
 import { MdReplay, MdSkipNext } from 'react-icons/md'
@@ -9,13 +9,14 @@ const EarTrainerFooter = () => {
     const {replayQuestion, playQuestion, setNumQs} = useEarTrainingContext();
     const [isSelectOpen, setIsSelectOpen] = useState(false);
     const [menuLocation, setMenuLocation] = useState({})
+    const settingsBtn = useRef(null)
     
     const displayMenu = (e) => {
-        const btn = e.target.getBoundingClientRect();
+        /* const btn = e.target.getBoundingClientRect();
         const left = btn.left
         const top = btn.top;
         const width = btn.width;
-        setMenuLocation({left, top, width});
+        setMenuLocation({left, top, width}); */
         setIsSelectOpen(!isSelectOpen);
     };
     const skipQuestion = () => {
@@ -23,18 +24,26 @@ const EarTrainerFooter = () => {
         setNumQs(prevstate => prevstate += 1);
 
     };
+    useEffect(() => {
+        const btn = settingsBtn.current.getBoundingClientRect();
+        const left = btn.left
+        const top = btn.top;
+        const width = btn.width;
+        setMenuLocation({left, top, width});
+        
+    }, [])
 
     return (
    <footer className="ear-trainer-footer">
+        <button ref={settingsBtn} className="ear-trainer-footer-btn">
+            <GiMusicalNotes onClick={displayMenu} />
+        </button> 
         <button className="ear-trainer-footer-btn replay-btn">
             <MdReplay onClick={replayQuestion}/>
         </button>
         <button className="ear-trainer-footer-btn">
             <MdSkipNext onClick={skipQuestion}/> 
         </button>
-        <button className="ear-trainer-footer-btn">
-            <GiMusicalNotes onClick={displayMenu} />
-        </button> 
         
         <SelectNotesModal location={menuLocation} isSelectOpen={isSelectOpen}/>
         <Score/>
