03c66009f0d8dd7167d937e432273a25a3cc42af
andy-little <69594249+andy-little@users.noreply.github.com>
2021-12-10 14:21:20 +0000

    added help content

--- src/components/HelpModal.js
+++ src/components/HelpModal.js
@@ -1,11 +1,25 @@
 import React from 'react'
-import { useEarTrainingContext } from '../EarTrainingContext'
+import { MdReplay, MdSkipNext } from 'react-icons/md'
+import {GiMusicalNotes} from 'react-icons/gi'
+import { useEarTrainingContext } from '../EarTrainingContext';
+
 
 const HelpModal = () => {
     const {isHelpOpen} = useEarTrainingContext();
     return (
         <article className={`help-modal ${isHelpOpen && 'show'}`}>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In incidunt unde mollitia vel minima velit, dolorum nobis autem vitae recusandae ab labore alias maxime impedit officiis aliquam nesciunt. Quas, eligendi.</p>
+            <h3 className='help-modal-header'>Getting Started</h3>
+
+            <p>Select a key and hit play. If you are new we recommend starting with C Major.</p>
+
+            <p>
+                Next click <GiMusicalNotes/> to choose which intervals you’d like to be tested on, <i>at least one note must be selected</i>. Try recognising the notes of the major triad before progressing to diatonic notes.
+            </p>
+
+            <p>Use <MdReplay/> to hear the question again or <MdSkipNext/> to skip a question and forfeit the point. </p>
+
+            <p>Click your score to toggle between percentage vs tally.</p>
+
         </article>
     )
 }
