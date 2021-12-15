5bafbe0a2ba11aa06b5b894826c77aa79662e3ea
andy-little <69594249+andy-little@users.noreply.github.com>
2021-12-15 14:24:40 +0000

    added tests for score component

---
+++ src/components/EarTrainerFooter/Score/__test__/Score.test.js
@@ -0,0 +1,27 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import Score from '../Score'
+import {EarTrainingContextProvider} from '../../../EarTrainingContext';
+
+const MockScore = () =>{
+  return(<EarTrainingContextProvider>
+    <Score/>
+  </EarTrainingContextProvider>)
+}
+
+test('test score renders to page', () => {
+  render(<MockScore/>);
+  const score = screen.queryByText(/score: \d+\/\d+/i);
+  expect(score).toBeInTheDocument();
+  fireEvent.click(score);
+  expect(score).not.toHaveTextContent(/score: \d+\/\d+/i);
+  expect(score).toHaveTextContent(/score: \d\%/i);
+});
+
+test('test score toggles between percentage & tally', () => {
+  render(<MockScore/>);
+  const score = screen.queryByText(/score: \d+\/\d+/i);
+  fireEvent.click(score);
+  expect(score).toHaveTextContent(/score: \d\%/i);
+  fireEvent.click(score);
+  expect(score).toHaveTextContent(/score: \d+\/\d+/i);
+});
