798c793bb27f1ab98d5bf3fe4397777e48baff46
andy-little <69594249+andy-little@users.noreply.github.com>
2021-12-17 11:22:15 +0000

    added intergration tests for start modal to app.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from './App'
 import {EarTrainingContextProvider} from './EarTrainingContext';
+//import { act } from 'react-dom/test-utils';
 
 const MockApp = () =>{
     
@@ -11,16 +12,21 @@ const MockApp = () =>{
     </EarTrainingContextProvider>)
 }
 
+beforeEach(()=>{
+  render(<MockApp/>);
+
+});
+
 describe('help btn & modal', () => {
     /* these tests would be better with toBeVissible but need to rearrange css first */
     test('by default modal should be hidden', () => {
-      render(<MockApp/>);
+      
       const helpModal = screen.getByTestId('help-modal');
       expect(helpModal).not.toHaveClass('show');
     });
     
     test('clicking btn should reveal modal', () => {
-      render(<MockApp/>);
+      
       const helpBtn = screen.getByRole('button', {name : 'help'});
       const helpModal = screen.getByTestId('help-modal');
       fireEvent.click(helpBtn);
@@ -28,7 +34,7 @@ describe('help btn & modal', () => {
     });
 
     test('clicking btn again should hide modal', () => {
-      render(<MockApp/>);
+      
       const helpBtn = screen.getByRole('button', {name : 'help'});
       const helpModal = screen.getByTestId('help-modal');
       fireEvent.click(helpBtn);
@@ -36,7 +42,7 @@ describe('help btn & modal', () => {
       expect(helpModal).not.toHaveClass('show');
     });
     test('clicking off modal should close modal', () => {
-      render(<MockApp/>);
+      
       const helpBtn = screen.getByRole('button', {name : 'help'});
       const helpModal = screen.getByTestId('help-modal');
       fireEvent.click(helpBtn);
@@ -45,3 +51,30 @@ describe('help btn & modal', () => {
     });
 
 });    
+
+describe('start modal integration', () => {
+
+  it('should exist when app loads', () => {
+    expect(screen.getByTestId('start-modal')).toBeInTheDocument();
+  });
+
+  it('should not close if no key selected', () => {
+    const startBtn = screen.getByRole('button', {  name: /start/i});
+    fireEvent.click(startBtn);
+    expect(screen.getByTestId('start-modal')).toBeInTheDocument();
+  });
+
+  it('should close if key IS selected', () => {
+    userEvent.selectOptions(
+      screen.getByRole('combobox'),
+      screen.getByRole('option', {name: 'C'}),
+    );
+    const startBtn = screen.getByRole('button', {  name: /start/i});
+    
+    act(() => {fireEvent.click(startBtn)});
+    
+    expect(screen.queryByTestId('start-modal')).not.toBeInTheDocument();
+  });
+  
+  
+});
