5f8f0cee80207f3707e9576eb3a3da3c5154c6ee
andy-little <69594249+andy-little@users.noreply.github.com>
2022-01-28 10:26:21 +0000

    added independent show/hide btn for display notes

--- src/components/EarTrainerFooter/EarTrainerFooter.js
+++ src/components/EarTrainerFooter/EarTrainerFooter.js
@@ -1,64 +1,93 @@
-import React, {useState, useRef, useEffect} from 'react'
+import React, { useState, useRef, useEffect } from "react";
 import Score from "./Score/Score";
-import SettingsModal from './SettingsModal/SettingsModal';
-import './EarTrainerFooter.scss';
-import {GiMusicalNotes} from 'react-icons/gi'
-import { MdReplay, MdSkipNext } from 'react-icons/md'
-import { useEarTrainingContext } from '../../EarTrainingContext';
+import SettingsModal from "./SettingsModal/SettingsModal";
+import "./EarTrainerFooter.scss";
+import { GiMusicalNotes } from "react-icons/gi";
+import { MdReplay, MdSkipNext } from "react-icons/md";
+import { BiShow, BiHide } from "react-icons/bi";
+import { useEarTrainingContext } from "../../EarTrainingContext";
 
 const EarTrainerFooter = () => {
-    const {replayQuestion, isSelectOpen, setIsSelectOpen, gameDispatch} = useEarTrainingContext();
-    
+    const {
+        replayQuestion,
+        isSelectOpen,
+        setIsSelectOpen,
+        gameDispatch,
+        isPianoDisplayNotes,
+    } = useEarTrainingContext();
+
     const [menuLocation, setMenuLocation] = useState({});
+
     const settingsBtn = useRef(null);
 
     const [size, setSize] = useState({});
-     const handler = () => {
-          /* size is only used to trigger getBtnordinates
+    const handler = () => {
+        /* size is only used to trigger getBtnordinates
             this seems backwards but fore some reason firing getBtnordinates in the actual event listener is buggy.
             particulary as it often doesn't work with orientation change */
-         setSize({height: window.innerHeight, width: window.innerWidth});
-     }
+        setSize({ height: window.innerHeight, width: window.innerWidth });
+    };
 
     const getBtnCordinates = () => {
         const btn = settingsBtn.current.getBoundingClientRect();
         const left = btn.left;
         const top = btn.top + 6;
         const width = btn.width - 6;
-        setMenuLocation({left, top, width});
-    }
+        setMenuLocation({ left, top, width });
+    };
     useEffect(() => {
         window.addEventListener("resize", handler);
         return () => {
-        window.removeEventListener("resize", handler);
-        }
+            window.removeEventListener("resize", handler);
+        };
     }, []);
 
-    useEffect(()=>{
+    useEffect(() => {
         getBtnCordinates();
-    },[size]);
-    
-   
+    }, [size]);
+
     const skipQuestion = () => {
-        gameDispatch({type: 'INCORRECT_ANSWER'});
+        gameDispatch({ type: "INCORRECT_ANSWER" });
     };
 
     return (
-   <footer className="ear-trainer-footer">
-        <button className="ear-trainer-footer-btn settings-btn" aria-label="settings" ref={settingsBtn} onClick={()=>setIsSelectOpen(!isSelectOpen)} >
-            <GiMusicalNotes />
-        </button> 
-        <button aria-label="replay" className="ear-trainer-footer-btn replay-btn" onClick={replayQuestion}>
-            <MdReplay/>
-        </button>
-        <button aria-label="skip" className="ear-trainer-footer-btn" onClick={skipQuestion}>
-            <MdSkipNext/> 
-        </button>
-        
-        <SettingsModal openBtn={settingsBtn} location={menuLocation}/>
-        <Score/>
-    </footer>
-    )
-}
+        <footer className="ear-trainer-footer">
+            <button
+                className="ear-trainer-footer-btn settings-btn"
+                aria-label="settings"
+                ref={settingsBtn}
+                onClick={() => setIsSelectOpen(!isSelectOpen)}
+            >
+                <GiMusicalNotes />
+            </button>
+            <button
+                className="ear-trainer-footer-btn show-hide-btn"
+                aria-label="show/hide"
+                onClick={() => {
+                    gameDispatch({ type: "TOGGLE_PIANO_DISPLAY_NOTES" });
+                }}
+            >
+                {isPianoDisplayNotes ? <BiHide /> : <BiShow />}
+            </button>
+            <button
+                aria-label="replay"
+                className="ear-trainer-footer-btn replay-btn"
+                onClick={replayQuestion}
+            >
+                <MdReplay />
+            </button>
+            <button
+                aria-label="skip"
+                className="ear-trainer-footer-btn"
+                onClick={skipQuestion}
+            >
+                <MdSkipNext />
+            </button>
 
-export default EarTrainerFooter
+            <SettingsModal openBtn={settingsBtn} location={menuLocation} />
+            <Score />
+        </footer>
+    );
+};
+
+export default EarTrainerFooter;
--- src/reducers/notes.js
+++ src/reducers/notes.js
@@ -1,56 +1,73 @@
 const ALPHABET = [
-    'an', 'as', 'bn', 'cn', 'cs', 'dn', 'ds', 'en', 'fn', 'fs', 'gn', 'gs',
+    "an",
+    "as",
+    "bn",
+    "cn",
+    "cs",
+    "dn",
+    "ds",
+    "en",
+    "fn",
+    "fs",
+    "gn",
+    "gs",
 ];
-const OCTAVES = ['2', '3'];
+const OCTAVES = ["2", "3"];
 
 export const notesDefaultState = {
     notes: ALPHABET,
     octaves: OCTAVES,
-    question: ''
+    question: "",
 };
 
 export const notesReducer = (state, action) => {
     switch (action.type) {
-        case 'SET_ALL': 
-            return {...state, notes: ALPHABET};
+        case "SET_ALL":
+            return { ...state, notes: ALPHABET };
 
-        case 'REMOVE_ALL': 
-            return {...state, notes: ['an']};
+        case "REMOVE_ALL":
+            return { ...state, notes: ["an"] };
 
-        case 'REMOVE_NOTE': 
+        case "REMOVE_NOTE":
             /* payload: str must be lowercase note followed by s for sharp or n for natural  */
             /* ADD ERROR CHECKING */
-            if(state.notes.length > 1){
-                const rmNotes = state.notes.filter(note => note !== action.payload);
-                return {...state, notes: rmNotes};
+            if (state.notes.length > 1) {
+                const rmNotes = state.notes.filter(
+                    (note) => note !== action.payload
+                );
+                return { ...state, notes: rmNotes };
             }
             return state;
-            
 
-        case 'ADD_NOTE': 
-            const isDuplicate = state.notes.find((item) => item === action.payload);
+        case "ADD_NOTE":
+            const isDuplicate = state.notes.find(
+                (item) => item === action.payload
+            );
 
-            if(isDuplicate){
+            if (isDuplicate) {
                 throw new Error(`Note: ${action.payload} already selected`);
-            };
-
-            const isNoteValid = ALPHABET.find((item) => item === action.payload);
-
-            if(!isNoteValid){
-                throw new Error(`${action.payload} is not a valid input to ADD_NOTE`);
             }
 
-            const newNotes = [...state.notes, action.payload]
+            const isNoteValid = ALPHABET.find(
+                (item) => item === action.payload
+            );
 
-            return {...state, notes: newNotes};
+            if (!isNoteValid) {
+                throw new Error(
+                    `${action.payload} is not a valid input to ADD_NOTE`
+                );
+            }
 
-        case 'SET_QUESTION': 
+            const newNotes = [...state.notes, action.payload];
+
+            return { ...state, notes: newNotes };
+
+        case "SET_QUESTION":
             /* MAYBE CHECK NOTE IS VALID */
             /* COULD WRITE REUSABLE FUNCTION FOR THIS */
-            return {...state, question: action.payload};
-            
-        default:
-            throw new Error('no matching action type');
+            return { ...state, question: action.payload };
 
-        }
-}
+        default:
+            throw new Error("no matching action type");
+    }
+};
